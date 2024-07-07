1d46ce1fcc55bcd32aa5f8c7f86a03d10af16783
midorikun53 <109507387+midorikun53@users.noreply.github.com>
2024-07-08 04:51:24 +0900

    completed

--- mysystem.c
+++ mysystem.c
@@ -10,15 +10,50 @@
 
 // system関数のクローン
 int mysystem(char *command) {
-  int status = 0;
-
-  // ここにプログラムを書く
+  int pid, status;
+  if (command == NULL)  // NULLポインタの場合
+    return 1;
+  if ((pid=fork()) < 0) // forkに失敗した場合
+    return -1;
+    
+  if (pid != 0) {
+    // 親プロセスの動作
+    while(wait(&status) != pid)
+      ;
+  } else {
+    // 子プロセスの動作
+    execl("/bin/sh", "sh", "-c", command, NULL);
+    perror("/bin/sh");
+    exit(127);
+  }
 
   return status;
 }
 
 /* 実行例
+  % make                    <- コンパイルエラーなし
+  cc -Wall -std=c99 -o mysysmain mysysmain.c  mysystem.c 
 
-ここに実行例を書く
+  % echo aaa                <- コマンドラインで実行
+  aaa
+
+  % ./mysysmain "echo aaa"  <- mysysmainで実行
+  mysystem:                 <- mysystemの結果
+  aaa
+  retval = 00000000
+  system:                   <- systemの結果
+  aaa
+  retval = 00000000
+
+  % ./mysysmain "rmdir nothing" <- 実行時エラー
+  mysystem:
+  rmdir: nothing: No such file or directory
+  retval = 00000100
+  system:
+  rmdir: nothing: No such file or directory
+  retval = 00000100
+
+  % ./mysysmain                             <- 引数不足
+  使い方 : ./mysysmain コマンド文字列          <- Usageの表示
 
 */
